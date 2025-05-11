b7e351636f9e705d8db9ee657ebc5d0fbd2168ae
2NH <89673213+mjhmojthu@users.noreply.github.com>
2025-05-11 17:26:21 +0700

    Add files via upload

---
+++ CPU/run_rl_swarm.sh
@@ -0,0 +1,215 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# General arguments
+ROOT=$PWD
+
+export PUB_MULTI_ADDRS
+export PEER_MULTI_ADDRS
+export HOST_MULTI_ADDRS
+export IDENTITY_PATH
+export CONNECT_TO_TESTNET
+export ORG_ID
+export HF_HUB_DOWNLOAD_TIMEOUT=120  # 2 minutes
+
+# Check if public multi-address is given else set to default
+DEFAULT_PUB_MULTI_ADDRS=""
+PUB_MULTI_ADDRS=${PUB_MULTI_ADDRS:-$DEFAULT_PUB_MULTI_ADDRS}
+
+# Check if peer multi-address is given else set to default
+DEFAULT_PEER_MULTI_ADDRS="/ip4/38.101.215.13/tcp/30002/p2p/QmQ2gEXoPJg6iMBSUFWGzAabS2VhnzuS782Y637hGjfsRJ" # gensyn coordinator node
+PEER_MULTI_ADDRS=${PEER_MULTI_ADDRS:-$DEFAULT_PEER_MULTI_ADDRS}
+
+# Check if host multi-address is given else set to default
+DEFAULT_HOST_MULTI_ADDRS="/ip4/0.0.0.0/tcp/38331"
+HOST_MULTI_ADDRS=${HOST_MULTI_ADDRS:-$DEFAULT_HOST_MULTI_ADDRS}
+
+# Path to an RSA private key
+DEFAULT_IDENTITY_PATH="$ROOT"/swarm.pem
+IDENTITY_PATH=${IDENTITY_PATH:-$DEFAULT_IDENTITY_PATH}
+
+SMALL_SWARM_CONTRACT="0x69C6e1D608ec64885E7b185d39b04B491a71768C"
+BIG_SWARM_CONTRACT="0x6947c6E196a48B77eFa9331EC1E3e45f3Ee5Fd58"
+
+# Will ignore any visible GPUs if set
+CPU_ONLY=${CPU_ONLY:-""}
+
+# Set if parsed from modal-login/temp-data/userData.json
+ORG_ID=${ORG_ID:-""}
+
+GREEN_TEXT="\033[32m"
+BLUE_TEXT="\033[34m"
+RESET_TEXT="\033[0m"
+
+echo_green() {
+    echo -e "$GREEN_TEXT$1$RESET_TEXT"
+}
+
+echo_blue() {
+    echo -e "$BLUE_TEXT$1$RESET_TEXT"
+}
+
+ROOT_DIR="$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)"
+
+# Function to clean up
+cleanup() {
+    echo_green ">> Shutting down trainer..."
+    rm -r $ROOT_DIR/modal-login/temp-data/*.json 2> /dev/null || true
+    kill -- -$$ || true
+    exit 0
+}
+
+trap cleanup EXIT
+
+echo -e "\033[38;5;224m"
+cat << "EOF"
+    ██████  ██            ███████ ██     ██  █████  ██████  ███    ███
+    ██   ██ ██            ██      ██     ██ ██   ██ ██   ██ ████  ████
+    ██████  ██      █████ ███████ ██  █  ██ ███████ ██████  ██ ████ ██
+    ██   ██ ██                 ██ ██ ███ ██ ██   ██ ██   ██ ██  ██  ██
+    ██   ██ ███████       ███████  ███ ███  ██   ██ ██   ██ ██      ██
+    From Gensyn
+EOF
+
+while true; do
+    echo -en $GREEN_TEXT
+    read -p ">> Would you like to connect to the Testnet? [Y/n] " yn
+    echo -en $RESET_TEXT
+    yn=${yn:-Y}
+    case $yn in
+        [Yy]*)  CONNECT_TO_TESTNET=true && break ;;
+        [Nn]*)  CONNECT_TO_TESTNET=false && break ;;
+        *)  echo ">>> Please answer yes or no." ;;
+    esac
+done
+
+while true; do
+    echo -en $GREEN_TEXT
+    read -p ">> Which swarm would you like to join (Math (A) or Math Hard (B))? [A/b] " ab
+    echo -en $RESET_TEXT
+    ab=${ab:-A}
+    case $ab in
+        [Aa]*)  USE_BIG_SWARM=false && break ;;
+        [Bb]*)  USE_BIG_SWARM=true && break ;;
+        *)  echo ">>> Please answer A or B." ;;
+    esac
+done
+if [ "$USE_BIG_SWARM" = true ]; then
+    SWARM_CONTRACT="$BIG_SWARM_CONTRACT"
+else
+    SWARM_CONTRACT="$SMALL_SWARM_CONTRACT"
+fi
+while true; do
+    echo -en $GREEN_TEXT
+    read -p ">> How many parameters (in billions)? [0.5, 1.5, 7, 32, 72] " pc
+    echo -en $RESET_TEXT
+    pc=${pc:-0.5}
+    case $pc in
+        0.5 | 1.5 | 7 | 32 | 72) PARAM_B=$pc && break ;;
+        *)  echo ">>> Please answer in [0.5, 1.5, 7, 32, 72]." ;;
+    esac
+done
+
+if [ "$CONNECT_TO_TESTNET" = true ]; then
+    # Run modal_login server
+    echo "Please login to create an Ethereum Server Wallet"
+    cd modal-login
+    yarn dev > /dev/null 2>&1 &
+    SERVER_PID=$!
+    echo "Started server process: $SERVER_PID"
+    sleep 5
+
+    if open http://localhost:3000 2> /dev/null; then
+        echo_green ">> Successfully opened http://localhost:3000 in your default browser."
+    else
+        echo ">> Failed to open http://localhost:3000. Please open it manually."
+    fi
+
+    cd ..
+
+    echo_green ">> Waiting for modal userData.json..."
+    while [ ! -f "modal-login/temp-data/userData.json" ]; do
+        sleep 5
+    done
+    echo "Found userData.json. Proceeding..."
+
+    ORG_ID=$(awk 'BEGIN { FS = "\"" } !/^[ \t]*[{}]/ { print $(NF - 1); exit }' modal-login/temp-data/userData.json)
+    echo "Your ORG_ID is set to: $ORG_ID"
+
+    echo "Waiting for API key to become activated..."
+    while true; do
+        STATUS=$(curl -s "http://localhost:3000/api/get-api-key-status?orgId=$ORG_ID")
+        if [[ "$STATUS" == "activated" ]]; then
+            echo "API key is activated! Proceeding..."
+            break
+        else
+            echo "Waiting for API key to be activated..."
+            sleep 5
+        fi
+    done
+
+    ENV_FILE="$ROOT"/modal-login/.env
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        sed -i '' "3s/.*/SMART_CONTRACT_ADDRESS=$SWARM_CONTRACT/" "$ENV_FILE"
+    else
+        sed -i "3s/.*/SMART_CONTRACT_ADDRESS=$SWARM_CONTRACT/" "$ENV_FILE"
+    fi
+fi
+
+# Set config based on GPU/CPU and swarm
+if [ -n "$CPU_ONLY" ] || ! command -v nvidia-smi &> /dev/null; then
+    CONFIG_PATH="$ROOT/hivemind_exp/configs/mac/grpo-qwen-2.5-0.5b-deepseek-r1.yaml"
+    GAME="gsm8k"
+else
+    case "$PARAM_B" in
+        32 | 72) CONFIG_PATH="$ROOT/hivemind_exp/configs/gpu/grpo-qwen-2.5-${PARAM_B}b-bnb-4bit-deepseek-r1.yaml" ;;
+        0.5 | 1.5 | 7) CONFIG_PATH="$ROOT/hivemind_exp/configs/gpu/grpo-qwen-2.5-${PARAM_B}b-deepseek-r1.yaml" ;;
+        *)  echo ">>> Invalid parameter count" && exit 1 ;;
+    esac
+    if [ "$USE_BIG_SWARM" = true ]; then
+        GAME="dapo"
+    else
+        GAME="gsm8k"
+    fi
+fi
+
+echo_green ">> Good luck in the swarm!"
+echo_blue ">> Post about rl-swarm on X/twitter! --> https://tinyurl.com/swarmtweet"
+echo_blue ">> And remember to star the repo on GitHub! --> https://github.com/gensyn-ai/rl-swarm"
+
+HF_TOKEN=${HF_TOKEN:-""}
+if [ -n "${HF_TOKEN}" ]; then
+    HUGGINGFACE_ACCESS_TOKEN=${HF_TOKEN}
+else
+    echo -en $GREEN_TEXT
+    read -p ">> Would you like to push models to Hugging Face Hub? [y/N] " yn
+    echo -en $RESET_TEXT
+    yn=${yn:-N}
+    case $yn in
+        [Yy]*) read -p "Enter your Hugging Face access token: " HUGGINGFACE_ACCESS_TOKEN ;;
+        [Nn]*) HUGGINGFACE_ACCESS_TOKEN="None" ;;
+        *) echo ">>> No answer, NO models will be pushed" && HUGGINGFACE_ACCESS_TOKEN="None" ;;
+    esac
+fi
+
+if [ -n "$ORG_ID" ]; then
+    python -m hivemind_exp.gsm8k.train_single_gpu \
+        --hf_token "$HUGGINGFACE_ACCESS_TOKEN" \
+        --identity_path "$IDENTITY_PATH" \
+        --modal_org_id "$ORG_ID" \
+        --contract_address "$SWARM_CONTRACT" \
+        --config "$CONFIG_PATH" \
+        --game "$GAME"
+else
+    python -m hivemind_exp.gsm8k.train_single_gpu \
+        --hf_token "$HUGGINGFACE_ACCESS_TOKEN" \
+        --identity_path "$IDENTITY_PATH" \
+        --public_maddr "$PUB_MULTI_ADDRS" \
+        --initial_peers "$PEER_MULTI_ADDRS" \
+        --host_maddr "$HOST_MULTI_ADDRS" \
+        --config "$CONFIG_PATH" \
+        --game "$GAME"
+fi
+
+wait
